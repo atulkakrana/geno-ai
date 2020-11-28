a7cd0c5c1c291206f31a272decffc436527b92a1
Atul Kakrana <atulkakrana@example.com>
2020-11-27 22:52:43 -0500

    updates from dips

--- analysis.py
+++ analysis.py
@@ -13,7 +13,7 @@ import pandas as pd
 # %%
 ## FUNCTIONS
 
-# %%
+# %% Single Cell Data
 dmat = mmread("data/sc-seq/e-enad-15/E-ENAD-15.aggregated_filtered_normalised_counts.mtx")
 # %%
 df  = pd.DataFrame.sparse.from_spmatrix(dmat)
---
+++ prepare_labels.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+## prepare labesl for genes
+## how to select high-confidence labelled gene set?
+## what will be the split proportion?
+
+# %% ENV VARIABLES
+import os, sys
+HOME = os.getenv("HOME")
+
+
+# %% IMPORTS
+import os
+import pandas as pd
+
+
+# %%
