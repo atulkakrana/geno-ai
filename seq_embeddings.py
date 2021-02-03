6b52bba8ea07e02ee406c9d828e4e27cccc3b07b
atulkakrana <atulkakrana@gmail.com>
2021-02-02 22:14:24 -0500

    Added basic seqeunce segementation

--- seq_embeddings.py
+++ seq_embeddings.py
@@ -1,5 +1,11 @@
-## this code computes embeddings 
-## for DNA seqeunces
+#!/usr/bin/env python
+
+## This code processes the 
+## DNA sequence data and 
+## trains seq embedidngs 
+## model for vectorizing
+## the DNA seqeunces (whole
+## genome or FASTA files)
 
 # %% ENVIRONMENT
 import os, sys
@@ -43,14 +49,52 @@ def fasta_reader(fas):
     print(f"\nRead file:{fas} | Seqeunces:{len(fasdct)}")
     return fasdct
 
-def process_genome_data(datadir, datafls):
+def fasta_writer(fas_dct, fas_out):
     '''
-    reads llist of fasta files; extacts seq and 
-    writes seq as newline seprated fasta seqeunce (one in each line)
+    writes fasta dict to fasta file;
+    the output filename is provided as an input
     '''
 
+    ## outfile
+    fhout = open(fas_out, 'w')
+
+    ## iterate and write
+    acount = 0
+    for head, seq in fas_dct.items():
+        fhout.write(">%s\n%s\n" % (head, seq))
+        acount+=1
+    fhout.close()
+
+    print(f"FASTA file with {acount} entried written:{fas_out}")
+    return fas_out
+
+def feats_writer(faslst, outfile):
+    '''
+    writes two element list (name, seq) to fasta
+    '''
     ## output
-    reslst = []
+    # outfile = "%s/all_features_seq.fa" % (datadir)
+    fhout = open(outfile, 'w')
+
+    ## write to file
+    acount = 0
+    for idx, aseq in enumerate(faslst):
+        fhout.write("%s\n\n" % (aseq))
+        acount +=1
+    fhout.close()
+    print(f"\nWrote {acount} seqs to file:{outfile}")
+    return None
+
+def combine_fasta(datadir, datafls):
+    '''
+    reads list of fasta files; extacts seq and 
+    writes seq as newline seprated fasta seqeunce (one in each line)
+    '''
+    print(f"\nFn: Combine FASTA Seqeunces")
+
+    ## output
+    reslst      = []
+    feats_out   = os.path.join(datadir, "all_features_seq.fa")
 
     acount = 0
     for fas in datafls:
@@ -65,28 +109,80 @@ def process_genome_data(datadir, datafls):
         reslst.extend(tmplst)
 
     ## write to file
-    _ = fasta_writer(reslst, datadir)
+    fas_dct = {idx:seq for idx,seq in enumerate(reslst)}
+    _ = fasta_writer(fas_dct, feats_out)
 
     print(f"Processed:{acount} fasta files")
     print(f"Total genomic features (i.e. seqeunces):{len(reslst)}")
-    return reslst
+    return reslst, feats_out
 
-def fasta_writer(faslst, datadir):
+def process_seqs(fas_in, method = "ok", out_format = "fasta"):
     '''
-    writes two element list (name, seq) to fasta
+    Perform FASTA segmentation or processing for 
+    embeddings.
+
+    Available Methods:
+    "ok": Overlapping, sliding, K-mer given a width and a stride input
+
+    Available file outputs (i.e. out_format):
+    "fasta" = fasta file (for pre-processing fasta before embeddings)
+    "feats" = text file with just line separated seqeunces (for training mebddings model)
     '''
+    print(f"\nFn: Seqeunce Segementator")
+
+    ## outputs
+    fas_dct     = {}
+    fas_out     = "%s-segmented.fas" % (fas_in.rpartition(".")[0])
+    feat_out    = "%s-segmented.feats"     % (fas_in.rpartition(".")[0])
+
+    ## inputs
+    fas_dct = fasta_reader(fas_in)
+
+    ## iterate and segment
+    if method == "ok":
+        for head, seq in fas_dct.items():
+            kmer_lst = overlap_k(seq, kwidth =3, stride = 1)
+            kmer_seq = " ".join(str(i) for i in kmer_lst)
+            fas_dct[head] = kmer_seq
+    else:
+        print(f"Method not implemented:{method} - exiting")
+        sys.exit()
+
+    ## write fasta file
+    if out_format   == "fasta":
+        _ = fasta_writer(fas_dct, fas_out)
+    elif out_format == "feats":
+        feat_lst = fas_dct.values()
+        _ = feats_writer(feat_lst, feat_out)
+    else:
+        print(f"The `out_format` value:{out_format} not recofnized - exiting")
+        sys.exit()
+
+    print(f"Sequence segementaion complete: {fas_out}")
+    return fas_dct, fas_out
+
+def overlap_k(seq, kwidth =3, stride = 1):
+    '''
+    sliding window for overlapping k-mers of given size after given strides
+    '''
+
     ## output
-    outfile = "%s/all_features_seq.fa" % (datadir)
-    fhout = open(outfile, 'w')
+    kmer_lst = []
 
-    ## write to file
-    acount = 0
-    for idx, aseq in enumerate(faslst):
-        fhout.write("%s\n\n" % (aseq))
-        acount +=1
-    fhout.close()
-    print(f"\nWrote {acount} seqs to file:{outfile}")
-    return None
+    ## size
+    owidth = int( ( ( len(seq.strip())-kwidth )/stride ) +1 )
+    # print(f"\nSeq Len:{len(seq)} | Output width:{owidth}")
+
+    idx = 0
+    for n in range(owidth):
+        s       = idx
+        kmer    = seq[s:s+kwidth]
+        kmer_lst.append(kmer)
+        idx     += stride
+
+    # print(f"Kmers snippet:{kmer_lst[:50]}")
+    # print(f"Seq length: {len(seq)} | Total Kmers:{len(kmer_lst)}")
+    return kmer_lst
 
 def train_word_vec_model(feature_file):
     '''
@@ -124,13 +220,20 @@ def train_word_vec_model(feature_file):
     return model
 
 # %% MAIN
-# out_fasta       = process_genome_data(DATA_DIR, DATA_FLS)
-# test_file       = "/home/atul/0.work/genomes/all_features_seq.fa"
-# test_file       = "/home/atul/0.work/genomes/Mus_musculus.GRCm38.68.dna.toplevel.forfasttext.fa"
-# wordvec_model   = train_word_vec_model(test_file)
+## Preapre Feats Seqeunce
+_, comb_fasta        = combine_fasta(DATA_DIR, DATA_FLS)
+train_fasta          = process_seqs(comb_fasta, out_format="feats")
+
+## Train Model
+# train_fasta           = "/home/atul/0.work/genomes/all_features_seq.fa"
+# train_fasta           = "/home/atul/0.work/genomes/Mus_musculus.GRCm38.68.dna.toplevel.forfasttext.fa"
+# wordvec_model         = train_word_vec_model(train_fasta)
 
 # %% DEV
 # model = fasttext.load_model("/home/atul/0.work/genomes/Mus_musculus.GRCm38.68.dna.toplevel.forfasttext_01_23_13_11.bin")
+
+# %%DEV
+process_seqs(fas_in = "test.fa", method = "ok")
 
 # %% TEST
 # vect = model.get_word_vector('ATGCGC')
@@ -154,6 +257,12 @@ if __name__ == "__main__":
 
 #### CHANGELOG
 ## v01 [01/21/2021]
+## wrote functions to read/write FASTA files and feature files for training
+## wrote functions for training FASTTEXT on DNA seqeunces
+
+## v01 -> v02 [02/01/2021]
+## added function to segment DNA seqeunces
+## reused `process_genome_data` to `combine_fasta` for downstream segemetation
 
 
 
